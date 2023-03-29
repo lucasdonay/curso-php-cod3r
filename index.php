3ad3004627e7179f4184af44e4f14fd74fab8118
Donay <donay@ingressolive.com>
2023-03-29 13:56:19 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <header>
+      <div class="container">
+          <h1>ESTUDOS DONAY PHP</h1>
+          <h2>Visualização dos conteúdos</h2>
+      </div>
+  </header>
+
+  <div class="container">
+      <div class="cards">
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+          <div class="card">
+              <h2>Tema da aula 01</h2>
+              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
+              </a>
+          </div>
+      </div>
+  </div>
+
+</body>
+
+</html>
