70bbe827725204d039eec3da84a122869d3f4616
Lucas <lucas.donay@gmail.com>
2023-03-29 22:56:21 -0300

    studies day 02

--- index.php
+++ index.php
@@ -24,39 +24,169 @@
   <div class="container">
       <div class="cards">
           <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
+              <h3>4. Controle</h3>
+              <ul>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=if_else">
+                          If Else
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=operadores_relacionais">
+                          Op. Relacionais
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=desafio_pi">
+                          Desafio PI
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=operadores_logicos">
+                          Operadores Lógicos
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">
+                          Desafio Op. Lógicos
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=operador_ternario">
+                          Operador Ternário
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=switch">
+                          Switch
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=controle&file=desafio_switch">
+                          Desafio Switch
+                      </a>
+                  </li>
+              </ul>
           </div>
           <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
+              <h3>3. Variáveis</h3>
+              <ul>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=basico">
+                          Variáveis
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=desafio_equacao">
+                          Desafio Equação
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=atribuicoes">
+                          Atribuições
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=interpolacao">
+                          Interpolação
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">
+                          Variáveis Variáveis
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=desafio_variaveis">
+                          Desafio Variáveis
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=valor_referencia">
+                          Valor vs Referência
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=variaveis&file=constantes">
+                          Constantes
+                      </a>
+                  </li>
+              </ul>
           </div>
           <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
+              <h3>2. Tipos</h3>
+              <ul>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=int">
+                          Tipo Inteiro
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=float">
+                          Tipo Float
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=aritmeticas">
+                          Op. Aritméticas
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=desafio_precedendia">
+                          Desafio Precedência
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=string">
+                          Tipo String
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=desafio_string">
+                          Desafio String
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=booleano">
+                          Tipo Booleano
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=tipos&file=conversoes">
+                          Conversões
+                      </a>
+                  </li>
+              </ul>
           </div>
           <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
-          </div>
-          <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
-          </div>
-          <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
-          </div>
-          <div class="card">
-              <h2>Tema da aula 01</h2>
-              <a href="exercicio.php?dir=teste&file=variavel">Link pra aula
-              </a>
+              <h3>1. Básico</h3>
+              <ul>
+                  <li>
+                      <a href="exercicio.php?dir=modulos/dir=basico&file=ola">
+                          Olá PHP
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=basico&file=html">
+                          Integração HTML
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=basico&file=css">
+                          Integração CSS
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=basico&file=comentarios">
+                          Comentários PHP
+                      </a>
+                  </li>
+                  <li>
+                      <a href="exercicio.php?dir=basico&file=desafio">
+                          Desafio
+                      </a>
+                  </li>
+              </ul>
           </div>
       </div>
   </div>
